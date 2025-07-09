2bd562eb0b196be6fe0a36ad895596030cfb4489
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-07-09 12:06:40 +0800

    Create Cave-Ambience.lua

---
+++ Cave-Ambience.lua
@@ -0,0 +1,12 @@
+--Cave ambience
+local sound = Instance.new("Sound")
+
+sound.SoundId = "rbxassetid://273398061"
+
+sound.Volume = 0.5
+
+sound.Looped = true
+
+sound:Play()
+
+sound.Parent = workspace
