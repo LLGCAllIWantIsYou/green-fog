bae4af835ed07dbed91e7117d6224a67d8ad9be2
FOCUS <focus_114514@qq.com>
2025-07-17 16:35:03 +0800

    Update MP3player.lua

--- MP3player.lua
+++ MP3player.lua
@@ -12,7 +12,6 @@ local function getGitSoundId(GithubSoundPath: string, AssetName: string): Sound
 end
 
 local C = getGitSoundId("https://github.com/Cosmella-v/Models/raw/main/video0-1-3.mp3", "All I Want Is You")--url(raw) and file name
-C.Parent = game.Workspace
-C.Looped = false
-C.Volume = 1
+C.Parent = workspace
+C.Volume = 0.25
 C:Play()
