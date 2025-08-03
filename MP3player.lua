a2e7d05646e4371d5d78801dbe50f8ee7e2f61f0
FOCUS <focus_114514@qq.com>
2025-08-03 20:04:38 +0800

    Update MP3player.lua

--- MP3player.lua
+++ MP3player.lua
@@ -13,6 +13,6 @@ end
 
 local C = getGitSoundId("https://github.com/Cosmella-v/Models/raw/main/video0-1-3.mp3", "All I Want Is You")--url(raw) and file name
 C.Parent = workspace
-C.Volume = 0.25
-C.Pitch = 0.5
+C.Volume = 0.1
+C.Pitch = 0.25
 C:Play()
